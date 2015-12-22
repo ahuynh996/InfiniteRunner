816224d84e1105a2b1e91b63a161bc8fc68ecac0
Eric Hua <Xelios96Sight@yahoo.com>
2015-12-22 14:38:49 -0500

    Dec. 22. 1015
    Today I worked on the player running animation and jump. Need to take jump sprite, and banicle

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,10 +11,19 @@ public class PlayerControl : MonoBehaviour {
 	
 	public float jumpHeight;
 	private bool grounded;
+	Animator anim;
+	public Sprite fruitSprites;
+
+
+
+	private Sprite sprite;
 	
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		 
+		 
 	}
 	
 	// Update is called once per frame
@@ -22,6 +31,10 @@ public class PlayerControl : MonoBehaviour {
 		grounded = Physics2D.IsTouchingLayers(this.gameObject.GetComponent<Collider2D>(), whatIsGround); 
 		if(Input.GetKeyDown(KeyCode.Space) && grounded){		
 			rigidbody.velocity = new Vector2(0f, jumpHeight);
+
+		
+
 		}	
+		anim.SetBool ("isGrounded", grounded);
 	}
 }
